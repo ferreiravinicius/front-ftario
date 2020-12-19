04d0e842322ba294c18dd338de5be1556ad0e827
Vinícius Ferreira <vinidotnet@gmail.com>
2020-12-19 16:33:01 -0300

    intial layout at finder

--- pages/finder.tsx
+++ pages/finder.tsx
@@ -1,25 +1,73 @@
+
+// boxShadow: `0 2px 5px 0 rgba(0,0,0,0.12),0 2px 10px 0 rgba(0,0,0,0.12)`,
 import { useStyletron } from "baseui";
 import { Block, BlockProps } from "baseui/block";
-import { Heading, HeadingLevel } from "baseui/heading";
+import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import {
-  Caption1,
-  H1,
-  HeadingMedium,
-  HeadingSmall,
-  Label1,
-  Paragraph3,
+  HeadingXSmall,
+  Paragraph2
 } from "baseui/typography";
 import * as React from "react";
-import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 
 export interface FinderProps {}
 
 const Finder: React.FC<FinderProps> = () => {
+  const [css, theme] = useStyletron();
+
+  const finderWrapper = css({
+    fontFamily: `Montserrat`,
+    display: "flex",
+    flexDirection: "column", //TODO: media query change to row @ mobile
+  });
+
+  const sideWrapper = css({
+    backgroundColor: COLORS.gray,
+    padding: ".5rem 2rem",
+  });
+
+  const gridWrapper = css({
+    backgroundColor: COLORS.gray,
+    padding: "0rem 2rem",
+  });
+
+  const mockInfo = css({
+    backgroundColor: COLORS.lightYellow,
+    borderRadius: "2rem",
+    padding: "2rem 1rem",
+    marginBottom: theme.sizing.scale800,
+  });
+
+  const mockIcon = css({
+    fontSize: "3rem",
+    padding: "0 1rem",
+  });
+
   return (
-    <>
-      <PlantGrid />
-    </>
+    <Block className={finderWrapper}>
+      <Block className={sideWrapper}>
+        <HeadingXSmall marginBottom={theme.sizing.scale800}>
+          Hello <strong>there</strong> 🌱{" "}
+        </HeadingXSmall>
+        <Block className={mockInfo}>
+          <Block display="flex" alignItems="center">
+            <Block className={mockIcon}>🌵</Block>
+            <Paragraph2 margin={0}>
+              How to maintain a haapy and healthy cactus.
+            </Paragraph2>
+          </Block>
+        </Block>
+      </Block>
+      <Block className={gridWrapper}>
+        <PlantGrid />
+      </Block>
+    </Block>
   );
+};
+
+export const COLORS = {
+  gray: "#F8F8F8",
+  white: "#FFFFFF",
+  lightYellow: "#FFEAC9",
 };
 
 export interface PlantGridProps {}
@@ -35,8 +83,7 @@ const PlantGrid: React.FC<PlantGridProps> = () => {
       flexGridColumnCount={[1, 2, 4, 6]}
       flexGridColumnGap="scale400"
       flexGridRowGap="scale400"
-      backgroundColor="#F8F8F8"
-      padding="2rem"
+      backgroundColor={COLORS.gray}
     >
       {[...Array(10)].map((_, item) => (
         <FlexGridItem key={item} {...itemProps}>
@@ -52,36 +99,32 @@ export interface PlantCardProps {}
 const PlantCard: React.FC<PlantCardProps> = () => {
   const [css, theme] = useStyletron();
 
-  const cardThemed = css({
+  const cardWrapper = css({
     width: "100%",
-    color: "#0C9769",
-    // color: "#E3F3EE",
-    overflow: "hidden",
-    padding: "2rem 0 0 0",
-    fontFamily: `Montserrat`,
+    color: "#0c0c0c",
     display: "flex",
     flexDirection: "column",
-    justifyItems: "center",
-    borderRadius: "1rem 4rem 0 0",
-    // backgroundColor: "#0C9769",
-    boxShadow: `0 2px 5px 0 rgba(0,0,0,0.12),0 2px 10px 0 rgba(0,0,0,0.12)`,
+    backgroundColor: COLORS.white,
+    padding: ".5rem",
+    borderRadius: "2rem"
+  });
 
-    background: `
-linear-gradient(135deg, #0D986A 25%, transparent 25%) -50px 0,
-linear-gradient(225deg, #0D986A 25%, transparent 25%) -50px 0,
-linear-gradient(315deg,#0D986A 25%, transparent 25%),
-linear-gradient(45deg, #0D986A 25%, transparent 25%);
-background-size: 100px 100px;
-background-color: #139e70;`
+  const topWrapper = css({
+    padding: "1rem",
+    borderRadius: "2rem",
+    backgroundColor: COLORS.gray,
+  });
+
+  const imgThemed = css({
+    width: "100%",
   });
 
   const contentThemed = css({
-    padding: "6rem 4rem 4rem 4rem",
-    borderRadius: "0rem 1rem 0 0",
-    // borderRadius: "4rem 4rem 0 0",
-    // borderRadius: "4rem 0rem 0 0",
-    backgroundColor: "#f3f3f3",
-    boxShadow: `0 2px 5px 0 rgba(0,0,0,0.12),0 2px 10px 0 rgba(0,0,0,0.12)`
+    backgroundColor: COLORS.white,
+    padding: "1rem 1.5rem",
+    "> span": {
+      backgroundColor: "red!important"
+    }
   });
 
   const titleThemed = css({
@@ -89,20 +132,9 @@ background-color: #139e70;`
     fontSize: "1.6rem",
   });
 
-  const imgThemed = css({
-    // border: "3px solid red",
-    width: "100%",
-  });
-
-  const topThemed = css({
-    // border: "3px solid red",
-    marginBottom: "-4rem",
-    zIndex: 1
-  });
-
   return (
-    <Block className={cardThemed}>
-      <Block className={topThemed}>
+    <Block className={cardWrapper}>
+      <Block className={topWrapper}>
         <img
           className={imgThemed}
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1145795/plant-1.png"
